fde63be6e64832c9452c513c8f46a7ed91dc1cb3
Niko Chaffin <niko@bixly.com>
2016-04-02 13:18:39 -0700

    reverted migration for casting options column on scoring field as text[]

--- db/migrate/20160325225257_change_options_format_on_scoring_field.rb
+++ db/migrate/20160325225257_change_options_format_on_scoring_field.rb
@@ -1,6 +1,6 @@
 class ChangeOptionsFormatOnScoringField < ActiveRecord::Migration
   def change
     # PostgreSQL needs a specific syntax for converting between column types
-    change_column :scoring_fields, :options, :text, array: true
+    change_column :scoring_fields, :options, "text[] USING CAST(options AS text[])"
   end
 end
