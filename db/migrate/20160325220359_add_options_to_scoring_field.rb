e032e137a2998ed4264bc5ece6193b642d32435a
Niko Chaffin <niko@bixly.com>
2016-04-02 14:08:46 -0700

    changed starting column type for options on scoring field

--- db/migrate/20160325220359_add_options_to_scoring_field.rb
+++ db/migrate/20160325220359_add_options_to_scoring_field.rb
@@ -1,6 +1,6 @@
 class AddOptionsToScoringField < ActiveRecord::Migration
   def change
-    add_column :scoring_fields, :options, :text
+    add_column :scoring_fields, :options, :string, array: true
     add_column :scoring_fields, :range_min, :integer
     add_column :scoring_fields, :range_max, :integer
   end
--- db/migrate/20160325225257_change_options_format_on_scoring_field.rb
+++
@@ -1,6 +0,0 @@
-class ChangeOptionsFormatOnScoringField < ActiveRecord::Migration
-  def change
-    # PostgreSQL needs a specific syntax for converting between column types
-    change_column :scoring_fields, :options, "text[] USING CAST(options AS text[])"
-  end
-end
